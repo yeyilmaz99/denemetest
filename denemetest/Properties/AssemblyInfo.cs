5d7cdba9ce90e022807270dcdfd0d52d00bf0829
yeyilmaz99 <yeyilmaz34@gmail.com>
2024-01-02 23:06:30 +0300

    Add project files.

---
+++ denemetest/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("denemetest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("denemetest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0c08a586-a9b4-4eab-8fd1-c5f4586510f1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ denemetest/WebService1.asmx.cs
@@ -0,0 +1,142 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Services;
+
+namespace denemetest
+{
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public List<DonenBrandList> MakeHttpRequest()
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    string apiUrl = "https://localhost:44345/api/Brands/getall";
+
+                    HttpResponseMessage response = client.GetAsync(apiUrl).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseData = response.Content.ReadAsStringAsync().Result;
+                        var result = JsonConvert.DeserializeObject<ApiResponse>(responseData);
+                        List<DonenBrandList> list = result.Data;
+                        return list;
+                    }
+                    else
+                    {
+                        List<DonenBrandList> list = new List<DonenBrandList>();
+                        return list;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                List<DonenBrandList> list = new List<DonenBrandList>();
+                return list;
+            }
+        }
+
+
+        [WebMethod]
+        public DonenBrandList MakeHttpRequestWithId(int id)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    string apiUrl = $"https://localhost:44345/api/Brands/getbyid?id={id}";
+                    HttpResponseMessage response = client.GetAsync(apiUrl).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseData = response.Content.ReadAsStringAsync().Result;
+                        var result = JsonConvert.DeserializeObject<SingleResponse>(responseData);
+                        DonenBrandList brand = result.Data;
+                        return result.Data;
+
+                    }
+                    else
+                    {
+                        DonenBrandList result = null;
+                        return result;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                DonenBrandList result = null;
+                return result;
+            }
+        }
+
+        [WebMethod]
+        public string MakeHttpPostRequest()
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    obj data =  new obj{ColorName="color1"};
+                    string apiUrl = "https://localhost:44345/api/Colors/add";
+                    var dataToSend = JsonConvert.SerializeObject(data);
+
+                    var httpContent = new StringContent(dataToSend, Encoding.UTF8, "application/json");
+
+                    HttpResponseMessage response = client.PostAsync(apiUrl, httpContent).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseData = response.Content.ReadAsStringAsync().Result;
+                        return responseData;
+                    }
+                    else
+                    {
+                        return "Error: " + response.StatusCode;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Error: " + ex.Message;
+            }
+        }
+    }
+
+    public class obj
+    {
+        public string ColorName;
+    }
+
+    public class DonenBrandList
+    {
+        public int BrandId { get; set; }
+        public string BrandName { get; set; }
+
+    }
+
+    public class SingleResponse
+    {
+        public DonenBrandList Data { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; }
+    }
+
+
+    public class ApiResponse
+    {
+        public List<DonenBrandList> Data { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; }
+    }
+}
